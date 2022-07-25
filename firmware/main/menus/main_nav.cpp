5dc8e64b6ea4254e8972422aea2a9e8a20eda0ae
cmdc0de <cmdc0dez@gmail.com>
2022-07-24 23:46:28 -0700

    main nav basics are working

--- firmware/main/menus/main_nav.cpp
+++ firmware/main/menus/main_nav.cpp
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <device/display/display_device.h>
+#include "device/display/color.h"
 #include "esp_netif_types.h"
 #include "main_nav.h"
 #include "menu_state.h"
@@ -13,9 +14,17 @@ using libesp::BaseMenu;
 
 static StaticQueue_t InternalQueue;
 static uint8_t InternalQueueBuffer[MainNav::QUEUE_SIZE*MainNav::MSG_SIZE] = {0};
+static libesp::DCImage MainMap;
 
 MainNav::MainNav() : AppBaseMenu() {
 	InternalQueueHandler = xQueueCreateStatic(QUEUE_SIZE,MSG_SIZE,&InternalQueueBuffer[0],&InternalQueue);
+   MainMap.bytes_per_pixel = 2;
+   MainMap.height = 128;
+   MainMap.width = 110;
+   extern char main_map_start[] asm("_binary_mainmap_map_start");
+   extern char main_map_end[]   asm("_binary_mainmap_map_end");
+   ESP_LOGI(LOGTAG,"size of mainmap %d",(main_map_end-main_map_start));
+   MainMap.pixel_data = &main_map_start[0];
 }
 
 MainNav::~MainNav() {
@@ -30,17 +39,14 @@ static INTERNAL_STATE InternalState = INIT;
 
 ErrorType MainNav::onInit() {
 	InternalState = INIT;
+   MyApp::get().getDisplay().fillScreen(RGBColor::BLACK);
 	return ErrorType();
 }
 
 BaseMenu::ReturnStateContext MainNav::onRun() {
 	BaseMenu::ReturnStateContext sr(this);
-	switch (InternalState) {
-		case INIT:
-			break;
-		case RENDER:
-			break;
-	}
+   MyApp::get().getDisplay().fillScreen(RGBColor::BLACK);
+   MyApp::get().getDisplay().drawImage(0,0,MainMap);
 	return sr;
 }
 
